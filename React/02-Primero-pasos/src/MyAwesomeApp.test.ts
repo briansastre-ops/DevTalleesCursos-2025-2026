b92c9cbcfcdba63c6be8c3b81b45cfdb2ed77521
Brian Sastre <briansastre159@gmail.com>
2025-11-21 10:30:50 -0300

    seguimos aprendiendo testing

--- React/02-Primero-pasos/src/MyAwesomeApp.test.ts
+++ React/02-Primero-pasos/src/MyAwesomeApp.test.ts
@@ -1,14 +1,28 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
-
-import { MyAwesomeApp } from "./MyAwesomeApp"; // Nota: Agregué llaves si MyAwesomeApp es una exportación nombrada
+import { MyAwesomeApp } from "./MyAwesomeApp";
 
 describe("MyAwesomeApp", () => {
   test("should render firstName and lastName", () => {
-    render(<MyAwesomeApp />);
+    const { container } = render(MyAwesomeApp);
 
     console.log(screen.debug());
+    const h1 = container.querySelector("h1");
+    const h3 = container.querySelector("h3");
+    console.log(h1);
 
+    expect(h1?.innerHTML).toContain("Fernando");
+    expect(h3?.innerHTML).toContain("Herrera");
     // console.log(container.innerHTML); // Línea comentada en la imagen
+  });
+  test("should render firstName and lastName", () => {
+    render(MyAwesomeApp);
+
+    screen.debug();
+    //const h1 = screen.getByRole("heading", {
+    //level: 1,
+    //});
+    const h1 = screen.getByTestId("first-name-title");
+    console.log(h1.innerHTML);
   });
 });
